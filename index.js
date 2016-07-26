30a3d570cbdb9164491afbb8d0ce8ba513bc1455
Chris Gardner <ctgardner89@gmail.com>
2016-07-26 17:00:17 -0400

    stopped here. not sure how to connect to mongodb

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require("express")
+var mongoose = require('mongoose')
 var app = express()
 app.set("view engine", "hbs")
-var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/recipes')
 var db = mongoose.connection;
 
 app.get('/', function(req, res){
-  
+  var recps = db.recipes.find();
+  res.send("<h1>recps</h1>");
 })
 
 app.listen(3030, function(){
