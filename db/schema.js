eb91544c77fd93ac8540b036b4c407e0ff9a70db
Chris Gardner <ctgardner89@gmail.com>
2016-07-26 16:33:29 -0400

    stopping point

---
+++ db/schema.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/recipes')
+
+var db = mongoose.connection;
+
+db.on('error', function(err){
+  console.log(err);
+});
+db.once('open', function() {
+  console.log("database has been connected!");
+});
+var Schema = mongoose.Schema
+var ObjectId = Schema.ObjectId
+
+var IngredientSchema = new Schema({
+  name: String,
+});
+
+var RecipeSchema = new Schema({
+  title: String,
+  ingredients:[IngredientSchema]
+});
+
+var Recipe = mongoose.model("Recipe", RecipeSchema);
+var Ingredient = mongoose.model("Ingredient", IngredientSchema);
+
+var omlette = new Recipe({title: "omlette"})
+var eggs = new Ingredient({name: "Eggs"})
+var cheese = new Ingredient({name: "Queso"})
+
+omlette.ingredients.push(eggs)
+omlette.ingredients.push(cheese)
+
+omlette.save(function(err, recipe){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log(recipe);
+  }
+})
+
+// {type: Schema.ObjectId, ref: "Recipe"}
+// console.log(omlette.ingredients);
